08d7f3ec87f3d37af0e01105ce5f4821b7b6da0e
DanielSBaumann <dr4wone@gmail.com>
2021-06-22 15:19:45 -0300

    Valores passados por referencia

---
+++ aula2/test3.js
@@ -0,0 +1,20 @@
+/**Primitivos e Referencia */
+/*
+const a = ['a', 'b', 'c']
+const b = a
+b.push('d')
+console.log(a, b)
+const c = [...a]
+c.push('XYZ')
+console.log(a, b, c)*/
+
+const aa = {
+    nome: 'Joao',
+    sobrenome: 'Silva'
+}
+const ab = aa
+ab.nome = 'Jose'
+console.log(aa, ab)
+const ac = { ...aa }
+ac.sobrenome = 'Nogueira'
+console.log(aa, ab, ac)
