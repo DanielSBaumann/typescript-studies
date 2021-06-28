eaa1fdf0c2535ff226d98ece569dd08df45d22cd
DanielSBaumann <dr4wone@gmail.com>
2021-06-28 18:15:52 -0300

    Exercicios

---
+++ aula7/exercicio1.js
@@ -0,0 +1,13 @@
+function max(n1, n2) {
+    return n1 > n2 ? n1 : n2;
+}
+
+/**assim */
+const max2 = (x, y) => {
+    return x > y ? x : y;
+}
+
+/**ou... */
+const max3 = (x, y) => x > y ? x : y;
+
+console.log(max3(7, 8));
